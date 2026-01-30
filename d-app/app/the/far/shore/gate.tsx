d0cc8c1211ba5366e77dfb7b8604aaac4d3ff857
Frisk202020 <156703513+Frisk202020@users.noreply.github.com>
2026-01-30 17:46:06 +0100

    resolve deployment suspense issue and rm lock file

---
+++ d-app/app/the/far/shore/gate.tsx
@@ -0,0 +1,17 @@
+import { createHash } from "crypto";
+import { useSearchParams } from "next/navigation";
+import FarShore from "./far_shore";
+import Lost from "./lost";
+
+const CORRECT_HASH = "abb764880ea7f113a40b2193ae320742a1f504e4508fb5a49863e351b04ecd36";
+
+export default function Gate() {
+    const params = useSearchParams();
+    const key = params.get("key");
+
+    const hash = createHash("sha256");
+    const digest = hash.update(key!).digest("hex");
+    return digest === CORRECT_HASH
+        ? <FarShore></FarShore>
+        : <Lost></Lost>;
+}
